a57714b47bb0d630eef97d4629e4ae3bc536b0a4
Fardi <fardi01.pro@gmail.com>
2022-04-29 14:51:36 +0200

    Initial Commit

---
+++ Habits_Tracker/Habits_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habits_TrackerApp.swift
+//  Habits_Tracker
+//
+//  Created by fardi Issihaka on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Habits_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
